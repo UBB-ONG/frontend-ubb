d4949174f2e1abd348c635b12663ab71097d1ac3
mrcs22 <marcusmoraes2010@hotmail.com>
2021-07-26 16:03:52 -0300

    feat(Landing): responsive layout

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -33,8 +33,6 @@ export default function Landing() {
         </div>
         <div>
           <iframe
-            width="600"
-            height="451"
             src="https://www.youtube.com/embed/7IxLNNFQD-I?controls=0"
             title="YouTube video player"
             allowFullScreen
@@ -55,31 +53,75 @@ const Content = styled.div`
   padding-top: 30px;
   padding-bottom: 296px;
 
+  iframe {
+    width: 100%;
+    height: 50vh;
+  }
+
   div {
-    height: 60vh;
-    width: 40%;
+    width: 50vw;
+    margin-left: 20px;
   }
 
   div:first-child {
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
-
-    padding-top: 80px;
+    justify-content: space-around;
+    width: 30vw;
+    height: 60vh;
   }
 
   img {
-    width: 250px;
-    height: 96px;
+    width: 20vw;
+
     cursor: pointer;
   }
 
   p {
     font-family: "Roboto", sans-serif;
     font-weight: 700;
-    font-size: 40px;
+    font-size: 2em;
     line-height: 47px;
 
     color: #1f2d3d;
+  }
+
+  @media (max-width: 1000px) {
+    p {
+      font-size: 1.5em;
+      line-height: 30px;
+    }
+  }
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+    padding-bottom: 180px;
+    iframe {
+      width: 90vw;
+    }
+
+    div {
+      width: 90vw;
+      margin: 0 auto;
+    }
+
+    div:first-child {
+      align-items: center;
+      width: 90vw;
+      height: 40vh;
+    }
+  }
+
+  @media (max-width: 620px) {
+    div:first-child {
+      height: 20vh;
+      margin-bottom: 20px;
+    }
+
+    p {
+      text-align: center;
+      font-size: 1em;
+      line-height: 1.2em;
+    }
   }
 `;
